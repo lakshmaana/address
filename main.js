e8e2fd6d92348ec0d08ac7b4437d8b747d4848bb
paavani <pavanisd9@gmail.com>
2017-05-23 10:01:12 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+(function(global) {
+
+    var store = global.store;
+    store.addContact(store.ContactInfo('Raj', '91122128889', 'raj@gmail.com'));
+    store.addContact(store.ContactInfo('Siva', '9331232213', 'siva@yahoo.com'));
+    store.addContact(store.ContactInfo('Ram', '9176461313', 'ram@hotmail.com'));
+
+}(this));
+
